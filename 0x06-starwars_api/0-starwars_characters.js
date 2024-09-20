ff3f12346663fb4e2cfb93aee21f813f9b6e77ac
Innocent Anozie <iygeal@gmail.com>
2024-09-20 23:11:12 +0100

    TASK 0: Star Wars Characters via Star Wars API

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,39 @@
+#!/usr/bin/node
+// Import the request module to make HTTP requests
+const request = require('request');
+
+// Get the movie ID from the command line arguments
+const movieId = process.argv[2];
+
+// Construct the API URL for the specific movie using the movie ID
+const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+
+// Make a request to the Star Wars API to get movie details
+request(apiUrl, function (error, response, body) {
+  if (error) {
+    console.error('Error:', error);
+    return;
+  }
+
+  // Parse the response body into JSON
+  const movieData = JSON.parse(body);
+
+  // Retrieve the character URLs from the movie data
+  const characterUrls = movieData.characters;
+
+  // Function to fetch each character's name
+  function fetchCharacterName(url) {
+    request(url, function (error, response, body) {
+      if (error) {
+        console.error('Error:', error);
+        return;
+      }
+      // Parse the character data and print the name
+      const characterData = JSON.parse(body);
+      console.log(characterData.name);
+    });
+  }
+
+  // Loop through the character URLs and fetch each character's name
+  characterUrls.forEach((url) => fetchCharacterName(url));
+});
