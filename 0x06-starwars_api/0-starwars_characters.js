b9616e9128fdbc45fd37fe553aea802b23e4da7b
Innocent Anozie <iygeal@gmail.com>
2024-09-21 00:07:34 +0100

    FIX: Make each request in sequence

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,36 +1,40 @@
 #!/usr/bin/node
-const argv = process.argv;
-const urlFilm = 'https://swapi-api.hbtn.io/api/films/';
-const urlMovie = `${urlFilm}${argv[2]}/`;
-
 const request = require('request');
 
-request(urlMovie, function (error, response, body) {
-  if (error == null) {
-    const fbody = JSON.parse(body);
-    const characters = fbody.characters;
+// Retrieve the movie ID from command-line arguments
+const movieId = process.argv[2];
 
-    if (characters && characters.length > 0) {
-      const limit = characters.length;
-      CharRequest(0, characters[0], characters, limit);
-    }
-  } else {
-    console.log(error);
-  }
-});
+// Construct the API URL for the specific movie
+const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
 
-function CharRequest (idx, url, characters, limit) {
-  if (idx === limit) {
-    return;
+// Function to fetch character data sequentially
+function fetchCharactersSequentially (characters, idx = 0) {
+  if (idx >= characters.length) {
+    return; // Stop when all characters are processed
   }
-  request(url, function (error, response, body) {
+
+  // Make request for the current character
+  request(characters[idx], (error, response, body) => {
     if (!error) {
-      const rbody = JSON.parse(body);
-      console.log(rbody.name);
-      idx++;
-      CharRequest(idx, characters[idx], characters, limit);
+      const characterData = JSON.parse(body);
+      console.log(characterData.name); // Print character name
+      // Move to the next character
+      fetchCharactersSequentially(characters, idx + 1);
     } else {
-      console.error('error:', error);
+      console.error('Error:', error);
     }
   });
 }
+
+// Make an API request to get the movie data
+request(apiUrl, (error, response, body) => {
+  if (error) {
+    console.error('Error:', error);
+  } else {
+    const movieData = JSON.parse(body);
+    const characters = movieData.characters;
+
+    // Start fetching character data sequentially
+    fetchCharactersSequentially(characters);
+  }
+});
